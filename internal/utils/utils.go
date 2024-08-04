86203a2078acbac1f170943affc27db284419455
aquilabot <jeancarlobenites@gmail.com>
2024-08-04 01:16:12 -0500

    Add initial implementation of URL matching utilities
    This commit introduces the go.mod and go.sum files to define module dependencies. Additionally, it includes initial utility functions for matching and extracting information from pcpartpicker URLs using regular expressions.

---
+++ internal/utils/utils.go
@@ -0,0 +1,68 @@
+package utils
+
+import (
+	"strings"
+
+	"github.com/dlclark/regexp2"
+)
+
+var (
+	pcppURLMatcher          = regexp2.MustCompile(`^(https?://)?([a-z]{2}\.)?pcpartpicker\.com(/.*)?$`, 0)
+	productURLMatcher       = regexp2.MustCompile(`^(https?://)?([a-z]{2}\.)?pcpartpicker\.com/product/[a-zA-Z0-9]{4,8}/[\S]*`, 0)
+	partListURLMatcher      = regexp2.MustCompile(`^(https?://)?([a-z]{2}\.)?pcpartpicker\.com/((list/[a-zA-Z0-9]{4,8})|((user/\w*/saved/(#view=)?[a-zA-Z0-9]{4,8})))`, 0)
+	vendorNameMatcher       = regexp2.MustCompile(`(?<=pcpartpicker\.com/mr/).*(?=\/)`, 0)
+	pcppUserSavedURLMatcher = regexp2.MustCompile(`^(https?://)?([a-z]{2}\.)?pcpartpicker\.com/user/[a-zA-Z0-9]*/saved/#view=[a-zA-Z0-9]{4,8}`, 0)
+)
+
+func ExtractVendorName(URL string) string {
+	if URL == "" {
+		return ""
+	}
+	m, err := vendorNameMatcher.FindStringMatch(URL)
+	if err != nil || m == nil {
+		return ""
+	}
+	return m.String()
+}
+
+func ConvertListURL(URL string) string {
+	match, _ := pcppUserSavedURLMatcher.MatchString(URL)
+
+	if !match {
+		return URL
+	}
+
+	return strings.Replace(URL, "#view=", "", 1)
+}
+
+func MatchPCPPURL(URL string) bool {
+	match, _ := pcppURLMatcher.MatchString(URL)
+
+	return match
+}
+
+func MatchProductURL(URL string) bool {
+	match, _ := productURLMatcher.MatchString(URL)
+
+	return match
+}
+
+func MatchPartListURL(URL string) bool {
+	match, _ := partListURLMatcher.MatchString(URL)
+
+	return match
+}
+
+func ExtractPartListURLs(text string) []string {
+	return Regexp2SearchAllText(partListURLMatcher, text)
+}
+
+func Regexp2SearchAllText(re *regexp2.Regexp, s string) []string {
+	var matches []string
+	m, _ := re.FindStringMatch(s)
+	for m != nil {
+		matches = append(matches, m.String())
+		m, _ = re.FindNextMatch(m)
+	}
+	return matches
+}
