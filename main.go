417159efb92b0a894f0a437e28598ea05731b749
aquilabot <jeancarlobenites@gmail.com>
2024-08-05 09:14:35 -0500

    Add main.go with PC parts search and detail endpoints
    Introduce a main application file using Fiber for web handling, incorporating endpoints for searching PC parts and fetching detailed part data. Error handling and logging are set up for better issue tracking and system behavior visibility.

---
+++ main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"errors"
+	"github.com/Aquilabot/KreaPC-API/pkg/scraper"
+	"github.com/gofiber/fiber/v2"
+
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/requestid"
+	"log"
+)
+
+type SearchRequest struct {
+	Query  string `json:"query"`
+	Region string `json:"region"`
+}
+
+type GetPartRequest struct {
+	URL string `json:"url"`
+}
+
+func main() {
+	// Initialize the scraper
+	scrap := scraper.NewScraper()
+	scrap.RandomizeUserAgent()
+
+	// Create a Fiber app
+	app := fiber.New()
+	app.Use(requestid.New())
+	app.Use(logger.New(logger.Config{
+		Format: "${pid} | ${time} | ${latency} | ${locals:requestid} | [${ip}]:${port} | ${status} - ${method} ${path}\n",
+	}))
+
+	// Endpoint for searching PC parts
+	app.Post("/search", func(c *fiber.Ctx) error {
+		var req SearchRequest
+		if err := c.BodyParser(&req); err != nil {
+			return c.Status(400).JSON(fiber.Map{"error": "Invalid request payload"})
+		}
+
+		searchResults, err := scrap.SearchPCParts(req.Query, req.Region)
+		if err != nil {
+			var redirectError *scraper.RedirectError
+			if errors.As(err, &redirectError) {
+				// Handle redirect to single product page
+				part, err := scrap.GetPart(redirectError.Error())
+				if err != nil {
+					return c.Status(500).JSON(fiber.Map{"error": "Error fetching product details"})
+				}
+				return c.JSON(part)
+			}
+			return c.Status(500).JSON(fiber.Map{"error": "Error searching parts"})
+		}
+
+		return c.JSON(searchResults)
+	})
+
+	// Endpoint for getting details of a single part
+	app.Post("/getPart", func(c *fiber.Ctx) error {
+		var req GetPartRequest
+		if err := c.BodyParser(&req); err != nil {
+			return c.Status(400).JSON(fiber.Map{"error": "Invalid request payload"})
+		}
+
+		part, err := scrap.GetPart(req.URL)
+		if err != nil {
+			return c.Status(500).JSON(fiber.Map{"error": "Error fetching part"})
+		}
+		return c.JSON(part)
+	})
+
+	// Start the server
+	log.Fatal(app.Listen(":3000"))
+}
